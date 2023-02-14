fe7295d36a3fdc4b784a0d6f37c05690dad45e39
S4mm4el <homer1996@gmail.com>
2023-02-14 13:14:31 +0300

    feat: add search at event page

--- src/pages/event-page/event-page.tsx
+++ src/pages/event-page/event-page.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { EventType } from 'types/Event.type';
+import { getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { eventsCol } from 'utils/firebase-config';
+import styles from './event-page.module.css';
 
 export function EventPage() {
-  const id = window.location.href.slice(-8);
+  const [events, setEvents] = useState<Array<EventType>>([]);
+  const [event, setEvent] = useState<EventType>();
 
-  return <div className="EventPage">{id}</div>;
+  useEffect(() => {
+    (async () => {
+      const eventsDocs = await getDocs(eventsCol);
+      setEvents(eventsDocs.docs.map((eventDoc) => eventDoc.data()));
+    })();
+  }, []);
+  useEffect(() => {
+    const id = window.location.href.slice(-8);
+    const getEvent = () => {
+      setEvent(events.find((x) => x.id === id));
+      //console.log(event?.1);
+    };
+    getEvent();
+  }, [events]);
+  return <div className={styles.EventPage}>{event?.name}</div>;
 }
