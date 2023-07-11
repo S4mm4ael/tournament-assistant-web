b9a0ac02d2bdcf282d62867853ac34701515810f
Semion Krapivin <93945710+S4mm4ael@users.noreply.github.com>
2023-07-11 14:05:12 +0000

    feat: add custom elo checkboxes and handlers

--- src/components/elo-input-card/elo-input-card.tsx
+++ src/components/elo-input-card/elo-input-card.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './elo-input-card.module.css';
 import { PlayerType } from 'types/Event.type';
@@ -35,19 +35,43 @@ export function EloInputCard({
   setPlayerOne,
   setPlayerTwo,
 }: EloInputCardProps) {
+
+  const [ifCustomFirst, setIfCustomFirst] = useState(false)
+  const [ifCustomSecond, setIfCustomSecond] = useState(false)
+
+  function handleCustomCheck(playerNum: number) {
+
+    if (playerNum === 1) {
+      ifCustomFirst ? setIfCustomFirst(false) : setIfCustomFirst(true)
+    }
+    else {
+      ifCustomSecond ? setIfCustomSecond(false) : setIfCustomSecond(true)
+    }
+    console.log()
+  }
+
+
   return <div className={styles.EloInputCard}>
     <div className={styles.EloInputCard__sideWrapper}>
       <h4>Select first Player</h4>
-      <PlayersDropdown></PlayersDropdown>
+      {ifCustomFirst ? <input type='number'
+        min="0"
+        max="3000"
+        className={styles.EloInputCard__customElo}></input> : <PlayersDropdown></PlayersDropdown>}
+      <div className=""><input type="checkbox" name="player-one-check" onChange={() => handleCustomCheck(1)} />
+        <label htmlFor="player-one-check"> Custom ELO</label><br></br></div>
 
-      <input type="checkbox" name="player-one-check" />
-      <label htmlFor="player-one-check"> Custom ELO</label><br></br>
     </div>
     <div className={styles.EloInputCard__sideWrapper}>
       <h4>Select second Player</h4>
-      <PlayersDropdown></PlayersDropdown>
-      <input type="checkbox" name="player-two-check" />
+      {ifCustomSecond ? <input type='number' min="0"
+        max="3000"
+        className={styles.EloInputCard__customElo}></input> : <PlayersDropdown></PlayersDropdown>}
+      <div className="">
+        <input type="checkbox" name="player-two-check" onChange={() => handleCustomCheck(2)} />
       <label htmlFor="player-two-check"> Custom ELO</label><br></br>
+      </div>      
+
     </div>
   </div>;
 }
