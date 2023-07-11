bdc127ebd03da1acfde2b01554f0e6a1541c6b14
Semion Krapivin <93945710+S4mm4ael@users.noreply.github.com>
2023-07-11 11:34:00 +0000

    feat: add props types for input elo component

--- src/components/elo-input-card/elo-input-card.tsx
+++ src/components/elo-input-card/elo-input-card.tsx
@@ -1,7 +1,38 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 
 import styles from './elo-input-card.module.css';
+import { PlayerType } from 'types/Event.type';
 
-export function EloInputCard() {
+type PlayerMockData = {
+  id: string;
+  elo: number;
+  firstname: string;
+  lastname: string;
+  nickname: string;
+  proxy?: undefined;
+} | {
+  id: string;
+  elo: number;
+  firstname: string;
+  lastname: string;
+  nickname: string;
+  proxy: boolean;
+}
+
+
+type EloInputCardProps = {
+  playersList: PlayerMockData[];
+  setPlayersList: React.Dispatch<React.SetStateAction<PlayerMockData[]>>;
+  setPlayerOne: React.Dispatch<React.SetStateAction<PlayerType>>;
+  setPlayerTwo: React.Dispatch<React.SetStateAction<PlayerType>>;
+};
+
+export function EloInputCard({
+  playersList,
+  setPlayersList,
+  setPlayerOne,
+  setPlayerTwo,
+}: EloInputCardProps) {
   return <div className={styles.EloInputCard}>Input here</div>;
 }
--- src/pages/elo-calculator/elo-calculator.tsx
+++ src/pages/elo-calculator/elo-calculator.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './elo-calculator.module.css';
 import { PairCard } from 'components/pair-card';
 import { PlayerType } from 'types/Event.type';
 import { EloInputCard } from 'components/elo-input-card';
+import playersData from '../../components/pairings-system/meme-cup-players.json';
 
 export function EloCalculatorPage() {
   const player1: PlayerType = {
     id: '1',
-    elo: 1490,
-    name: 'Marat',
+    elo: 1600,
+    name: 'First player',
     primary: 0,
     to: 0,
     toOpponents: 0,
@@ -16,19 +17,31 @@ export function EloCalculatorPage() {
   };
   const player2: PlayerType = {
     id: '2',
-    elo: 1390,
-    name: 'Second',
+    elo: 1600,
+    name: 'Second Player',
     primary: 0,
     to: 0,
     toOpponents: 0,
     vp: 0,
   };
+  const [playersList, setPlayersList] = useState(playersData);
+  const [playerOne, setPlayerOne] = useState<PlayerType>(player1);
+  const [playerTwo, setPlayerTwo] = useState<PlayerType>(player2);
+
+  useEffect(() => {
+    console.log(playersList);
+  }, playersList);
 
   return (
     <div className={styles.EloCalculatorPage}>
       Here you can easily count your ELO after game.
-      <EloInputCard></EloInputCard>
-      <PairCard player1={player1} player2={player2}></PairCard>
+      <EloInputCard
+        playersList={playersList}
+        setPlayersList={setPlayersList}
+        setPlayerOne={setPlayerOne}
+        setPlayerTwo={setPlayerTwo}
+      ></EloInputCard>
+      <PairCard player1={playerOne} player2={playerTwo}></PairCard>
     </div>
   );
 }
